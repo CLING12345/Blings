2edac87c4d1225a1a7f25e45ba65037ed67c6e10
Chuanlin.Xie <Chuanlin.Xie@geely.com>
2020-08-28 14:39:15 +0800

    咨询师增加骨架图

--- src/components/consultant/index.vue
+++ src/components/consultant/index.vue
@@ -1,16 +1,22 @@
 <template>
     <section>
-         <van-cell title="多个日期" :value="date" @click="show = true" />
-         <van-calendar type="range" v-model="show" @confirm="onConfirm" :show-confirm="false"  />
+        <div v-if="isShow">  
+            <van-cell title="多个日期" :value="date" @click="show = true" />
+            <van-calendar type="range" v-model="show" @confirm="onConfirm" :show-confirm="false"  />
+        </div>
+        <van-skeleton title :row="19" :loading="loading" /> 
     </section>
 </template>
 <script>
 import { Calendar,Cell } from 'vant'; 
+import { Skeleton } from 'vant';  
 export default {
     data(){
         return{
             date: '',
             show: false,
+            loading: true,
+            isShow: false
         }
     },
     methods:{
@@ -25,9 +31,16 @@ export default {
             this.date = `${this.formatDate(start)} - ${this.formatDate(end)}`;
         }
     },
+    mounted(){
+        setTimeout(()=>{
+            this.loading = false;
+            this.isShow = true;
+        },1000);
+    },
     components:{
         [Calendar.name]: Calendar,
-        [Cell.name]: Cell
+        [Cell.name]: Cell,
+        [Skeleton.name]: Skeleton
     }
 }
 </script>>
